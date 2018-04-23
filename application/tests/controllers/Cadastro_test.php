7e7cf4ff1330efe33fd5dc119f592921c999e385
Franklin-Barreto <franklin-barreto@hotmail.com>
2018-04-23 19:59:58 -0300

    testes validacoes

--- application/tests/controllers/Cadastro_test.php
+++ application/tests/controllers/Cadastro_test.php
@@ -3,16 +3,38 @@
 
 class Cadastro_test extends TestCase
 {
-   /* public function test_index()
-    {
-        $output = $this->request('GET', 'cadastro/index');
-        $this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
-    }*/
+
     
-    public function test_adicionarPaciente_falha(){
+    public function test_adicionarPaciente_nome_obrigatorio_falha(){
+        $_POST['nome'] = '';
+        $output = $this->request('POST', 'cadastro/adicionarPaciente',$_POST);
+        $output =json_decode($output);
+        $this->assertEquals(FALSE, $output->status);
+        
+    }
+    public function test_adicionarPaciente_tres_letras_iguais_falha(){
         $_POST['nome'] = 'Fraaanklin';
-        $_POST['nome_mae'] = 'Fraaanklin';
-        $_POST['email'] = 'franklin-barreto@hotmail.com';
+        $output = $this->request('POST', 'cadastro/adicionarPaciente',$_POST);
+        $output =json_decode($output);
+        $this->assertEquals(FALSE, $output->status);
+        
+    }
+    public function test_adicionarPaciente__mae_nome_obrigatorio_falha(){
+        $_POST['nome_mae'] = '';
+        $output = $this->request('POST', 'cadastro/adicionarPaciente',$_POST);
+        $output =json_decode($output);
+        $this->assertEquals(FALSE, $output->status);
+        
+    }
+    public function test_adicionarPaciente_email_obrigatorio(){
+        $_POST['email'] = '';
+        $output = $this->request('POST', 'cadastro/adicionarPaciente',$_POST);
+        $output =json_decode($output);
+        $this->assertEquals(FALSE, $output->status);
+        
+    }
+    public function test_adicionarPaciente_email_valido_falha(){
+        $_POST['email'] = 'franklin.com.br';
         $output = $this->request('POST', 'cadastro/adicionarPaciente',$_POST);
         $output =json_decode($output);
         $this->assertEquals(FALSE, $output->status);
