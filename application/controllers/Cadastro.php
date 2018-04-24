d414354937c4ccec23bc861297b6356b08e7f706
Franklin-Barreto <franklin-barreto@hotmail.com>
2018-04-24 09:12:35 -0300

    correção teste

--- application/controllers/Cadastro.php
+++ application/controllers/Cadastro.php
@@ -37,14 +37,6 @@ class Cadastro extends CI_Controller
 
     public function index()
     {
-        /*$params['limit'] = RECORDS_PER_PAGE;
-        $params['offset'] = ($this->input->get('per_page')) ? $this->input->get('per_page') : 0;
-        
-        $config = $this->config->item('pagination');
-        $config['base_url'] = site_url('cadastro/index?');
-        $config['total_rows'] = $this->Paciente_model->contar_todos_pacientes();
-        $this->pagination->initialize($config);*/
-        
         $data['pacientes'] = $this->Paciente_model->listar_pacientes();
         
         $this->load->view('cadastro_view', $data);
@@ -52,7 +44,7 @@ class Cadastro extends CI_Controller
 
     public function adicionarPaciente()
     {
-        $this->form_validation->set_rules('nome', 'Nome', 'validar_nome');
+        $this->form_validation->set_rules('nome', 'Nome', 'required|validar_nome');
         $this->form_validation->set_rules('nome_mae', 'Nome da mãe', 'required');
         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
         
@@ -87,7 +79,7 @@ class Cadastro extends CI_Controller
 
     public function atualizarPaciente()
     {
-        $this->form_validation->set_rules('nome', 'Nome', 'validar_nome');
+        $this->form_validation->set_rules('nome', 'Nome', 'required|validar_nome');
         $this->form_validation->set_rules('nome_mae', 'Nome da mãe', 'required');
         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
         
--- application/tests/controllers/Cadastro_test.php
+++ application/tests/controllers/Cadastro_test.php
@@ -6,6 +6,13 @@ class Cadastro_test extends TestCase
     public function test_adicionarPaciente_nome_obrigatorio_falha()
     {
         $_POST['nome'] = '';
+        $_POST['nome_mae'] = 'Francisca Pereira da Silva';
+        $_POST['nome_pai'] = 'Francisca Alves Barreto';
+        $_POST['status'] = 1;
+        $_POST['email'] = 'franklin-barreto@hotmail.com';
+        $_POST['nome_bairro'] = 'Jardim Marilia';
+        $_POST['rua'] = 'Pedro de Mena, 76';
+         
         $output = $this->request('POST', 'cadastro/adicionarPaciente', $_POST);
         $output = json_decode($output);
         $this->assertEquals(FALSE, $output->status);
@@ -14,6 +21,13 @@ class Cadastro_test extends TestCase
     public function test_adicionarPaciente_tres_letras_iguais_falha()
     {
         $_POST['nome'] = 'Fraaanklin';
+        $_POST['nome_mae'] = 'Francisca Pereira da Silva';
+        $_POST['nome_pai'] = 'Francisca Alves Barreto';
+        $_POST['status'] = 1;
+        $_POST['email'] = 'franklin-barreto@hotmail.com';
+        $_POST['nome_bairro'] = 'Jardim Marilia';
+        $_POST['rua'] = 'Pedro de Mena, 76';
+         
         $output = $this->request('POST', 'cadastro/adicionarPaciente', $_POST);
         $output = json_decode($output);
         $this->assertEquals(FALSE, $output->status);
@@ -21,7 +35,14 @@ class Cadastro_test extends TestCase
 
     public function test_adicionarPaciente__mae_nome_obrigatorio_falha()
     {
+        $_POST['nome'] = 'Franklin Pereira Barreto';
         $_POST['nome_mae'] = '';
+        $_POST['nome_pai'] = 'Francisca Alves Barreto';
+        $_POST['status'] = 1;
+        $_POST['email'] = 'franklin-barreto@hotmail.com';
+        $_POST['nome_bairro'] = 'Jardim Marilia';
+        $_POST['rua'] = 'Pedro de Mena, 76';
+         
         $output = $this->request('POST', 'cadastro/adicionarPaciente', $_POST);
         $output = json_decode($output);
         $this->assertEquals(FALSE, $output->status);
@@ -29,7 +50,14 @@ class Cadastro_test extends TestCase
 
     public function test_adicionarPaciente_email_obrigatorio()
     {
+        $_POST['nome'] = 'Franklin Pereira Barreto';
+        $_POST['nome_mae'] = 'Francisca Pereira da Silva';
+        $_POST['nome_pai'] = 'Francisca Alves Barreto';
+        $_POST['status'] = 1;
         $_POST['email'] = '';
+        $_POST['nome_bairro'] = 'Jardim Marilia';
+        $_POST['rua'] = 'Pedro de Mena, 76';
+        
         $output = $this->request('POST', 'cadastro/adicionarPaciente', $_POST);
         $output = json_decode($output);
         $this->assertEquals(FALSE, $output->status);
@@ -37,7 +65,12 @@ class Cadastro_test extends TestCase
 
     public function test_adicionarPaciente_email_valido_falha()
     {
+        $_POST['nome'] = 'Franklin Pereira Barreto';
+        $_POST['nome_mae'] = 'Francisca Pereira da Silva';
+        $_POST['nome_pai'] = 'Francisca Alves Barreto';
+        $_POST['status'] = 1;
         $_POST['email'] = 'franklin.com.br';
+        $_POST['nome_bairro'] = 'Jardim Marilia';
         $output = $this->request('POST', 'cadastro/adicionarPaciente', $_POST);
         $output = json_decode($output);
         $this->assertEquals(FALSE, $output->status);
